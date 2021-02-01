7dbaddbae29843f51ab196aa75700f7be13edf1e
Rohan <sabalerohan73@gmail.com>
2021-02-01 21:24:28 +0530

    commit

--- Practice/CPPEASE/Code chef/binary_subsequence.cpp
+++ Practice/CPPEASE/Code chef/binary_subsequence.cpp
@@ -94,4 +94,49 @@ Example case 5: We can remove s1
 */
 
 
-#include<bits
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+
+        int tot0 = 0, tot1 = 0;
+        for(auto c: s)
+        {
+            if (c =='0')
+            {
+                tot0++;
+            }
+            else
+            {
+                tot1++;
+            }
+            
+
+        }
+
+        int ans = min(tot0,tot1);
+        int curr0 = 0, curr1 = 0;
+        for(auto c: s)
+        {
+            if(c=='0')
+            {
+                curr0++;
+            }
+            else
+            {
+                curr1++;
+            }
+            ans = min(ans, curr1 + (tot0 - curr0));
+            
+        }
+        cout<<ans<<endl;
+    }
+}
