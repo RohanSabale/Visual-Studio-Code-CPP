4afb241a57e4706c73b0dc5c97ca14746e51e32c
Rohan <sabalerohan73@gmail.com>
2021-02-05 11:10:33 +0530

    commit

--- Practice/tricks/hidden.cpp
+++ Practice/tricks/hidden.cpp
@@ -1,7 +1,27 @@
+/*
+Swapping two number without using third variable it is using xor operator
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int arr[5] ={1,2,3,4,5};
-    
+    int a,b;
+    a=5;
+    b=6;
+
+    a^=b;
+    b^=a;
+    a^=b;
+
+    cout<<a<<b;
+
+}
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"rohan";
 }
