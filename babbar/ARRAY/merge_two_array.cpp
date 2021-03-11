ad529069112e29128078a8b6a40f77309d973ec4
Rohan <sabalerohan73@gmail.com>
2021-03-11 17:25:55 +0530

    commit

--- babbar/ARRAY/merge_two_array.cpp
+++ babbar/ARRAY/merge_two_array.cpp
@@ -1,2 +1,26 @@
-// merge two sorted array without using extra space 
+#include<bits/stdc++.h>
+#define boost ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
+using namespace std;
 
+int main()
+{
+
+    boost;
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt" ,"r" ,stdin);
+    freopen("output.txt" , "w" ,stdout);
+    #endif
+
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0 ;i<n ;i++)
+    {
+        cin>>arr[i];
+    }
+
+    bool hash[n];
+    memset(hash,true,n);
+   
+
+}
