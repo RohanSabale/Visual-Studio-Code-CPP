582c22900b3e64b858ff1703a144eb449993aff0
Rohan <sabalerohan73@gmail.com>
2021-04-19 12:14:17 +0530

    commit

---
+++ babbar/Searching and Sorting/rotated_sorted_array.cpp
@@ -0,0 +1,42 @@
+// Given the array nums after the rotation 
+//find the pivot element
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n = 7;
+    int arr[n] = {4,5,6,7,0,1,2};
+    int x= 4;
+
+    // first we have to find the pivot of array first
+
+    // this below loop below will give the pivot in O(n) time.
+    // for(int i =0;i<n;i++)
+    // {
+    //     if(arr[i] >arr[i+1])
+    //     {
+    //         cout<<arr[i+1];
+    //     }
+    // }
+
+    // for finding the pivot in O(logn) time we can use binary search
+    int low = 0;
+    int high = n-1;
+    
+    while(low<high)
+    {
+        int mid =(low +high)/2;
+        if(arr[mid] <arr[high])
+        {
+            high  = mid;
+        }
+        else if (arr[mid] >arr[high])
+        {
+            low = mid +1;
+        }
+    }
+    cout<<arr[low];
+    //cout<<arr[high];           both is same
+}
