c5aad392ed331b3389c89b0e66b2b988e7936186
Rohan <sabalerohan73@gmail.com>
2021-05-21 18:32:36 +0530

    commit

--- running_sum.cpp
+++ running_sum.cpp
@@ -5,5 +5,14 @@ int main()
 {
     vector<int>nums{1,2,1,1,3};
 
-    
+    vector<int>ans(nums.size()+1);
+    int count =0;
+    for(auto i : nums)
+    {
+        count += ans[i]++;
+    }
+    for(auto i : ans)
+    {
+        cout<<i<<endl;
+    }
 }
