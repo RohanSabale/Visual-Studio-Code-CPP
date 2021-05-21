40d220e91a78fd1692111141c748ce2783ec8a2b
Rohan <sabalerohan73@gmail.com>
2021-05-21 12:25:00 +0530

    commit

--- running_sum.cpp
+++ running_sum.cpp
@@ -1,17 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int solve(vector<int>&nums)
-{
-    int n = nums.size();
-    vector<int>out;
-    out[0] = nums[0];
-    return out[0];
-    
-}
-
 int main()
 {
-    vector<int>nums{1,2,3,4};
-    cout<<solve(nums);
+    vector<int>nums{1,2,1,1,3};
+
+    
 }
