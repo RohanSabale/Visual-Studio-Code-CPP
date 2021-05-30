4a6c51dc72a3182f744bbb1fd5c6c6276fa9feb3
Rohan <sabalerohan73@gmail.com>
2021-05-30 10:47:22 +0530

    commit

--- babbar/Trees/newtree.cpp
+++ babbar/Trees/newtree.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 struct Node
 {
     /* data */
@@ -62,7 +63,148 @@ Node* InsertNode(Node* root , int data)
     }
 }
 
-void printInorder(Node * temp)       // left , root,right
+// function to delete the given deepest node(d_node) 
+// in binary tree
+
+void deletDeepest(struct Node*root , struct Node*d_node)
+{
+    queue<struct Node *>q;
+    q.push(root);
+
+    // do level order traversal until last node
+    struct Node*temp;
+    while(!q.empty())
+    {
+        temp = q.front();
+        q.pop();
+        if(temp == d_node)
+        {
+            temp == NULL;
+            delete(d_node);
+            return;
+        }
+        if(temp->right)
+        {
+            if(temp->right ==d_node)
+            {
+                temp->right = NULL;
+                delete(d_node);
+                return;
+            }
+            else
+            {
+                q.push(temp->right);
+            }
+        }
+        if(temp->left)
+        {
+            if(temp->left == d_node)
+            {
+                temp->left =NULL;
+                delete(d_node);
+                return;
+            }
+            else
+            {
+                q.push(temp->left);
+            }
+        }
+    }
+}
+
+
+// function to delete element in binary tree
+Node* deletion(struct Node* root , int key)
+{
+    if(root == NULL)
+        return NULL;
+    
+    if(root->left == NULL && root->right  ==NULL)
+    {
+        if(root ->data == key)
+            return NULL;
+        else
+            return root;
+    }
+
+    queue<struct Node*> q;
+    q.push(root);
+
+    struct Node* temp;
+    struct Node*key_node = NULL;
+
+    // do level order traversal to find deepest node(temp)
+    // and node to be deleted(key_node)
+
+    while(!q.empty())
+    {
+        temp = q.front();
+        q.pop();
+
+        if(temp->data == key)
+            key_node = temp;
+        
+        if(temp->left)
+            q.push(temp->left);
+        
+        if(temp->right)
+            q.push(temp->right);
+    }
+    if(key_node!=NULL)
+    {
+        int x =temp->data;
+        deletDeepest(root , temp);
+        key_node->data = x;
+    }
+    return root;
+    
+}
+
+
+// all traversal require O(n) time 
+//breadth first traversal(or level order traversal)
+// space required is O(w) where w is maximum widhth of binary tree
+
+// Points
+// depth first traversal are typically recursive and recursive code requires
+// function call overhead
+// bfs starts visiting node from root while DFS starts visiting node from leaves.
+// so if we have to search something that is more likely to closer to root , 
+// we would prefer BFS, and if the target node is close to a leaf , we would prefer DFS
+
+
+// all these  3 are depth first traversal where h is maximum height of Binary tree
+// space requires is O(h) 
+
+void printLevelOrder( Node* root)
+{
+    if(root == NULL)
+        return;
+    
+    // create an empty queue for level order traversal
+    queue<Node*> q;
+
+    // ensure root and initialize height
+    q.push(root);
+
+    while(q.empty() == false)
+    {
+        // print front of queue and remove if from queue
+        Node * node  = q.front();
+        cout<<node->data<<" ";
+        q.pop();
+
+        //enqueue left child
+        if(node->left != NULL)
+            q.push(node->left);
+
+        // enqueue right child
+        if(node->right != NULL)
+            q.push(node->right);
+    }
+}
+void printInorder(Node * temp)       // left , root,right  
+
 {
     if(temp == NULL)
         return;
@@ -71,7 +213,7 @@ void printInorder(Node * temp)       // left , root,right
     printInorder(temp->right);
 }
 
-void printPreorder(struct Node* node)
+void printPreorder(struct Node* node)   // root,left ,right
 {
     if (node == NULL)
         return;
@@ -86,7 +228,7 @@ void printPreorder(struct Node* node)
     printPreorder(node->right);
 }
 
-void printPostorder(struct Node* node)
+void printPostorder(struct Node* node)  //left , right ,root
 {
     if (node == NULL)
         return;
@@ -109,19 +251,27 @@ int main()
     root->left->left = CreateNode(4);
     root->left->right = CreateNode(5);
     root->right->left = CreateNode(6);
-    //root->right->right = CreateNode(7);
+    root->right->right = CreateNode(7);
 
-    cout<<"inorder traversal before insertion   ";
-    printInorder(root);
-    cout<<endl;
+    // cout<<"inorder traversal before insertion   ";
+    // printInorder(root);
+    // cout<<endl;
 
-    int key  =  7;
-    root = InsertNode(root,key);
-    cout<<"Inorder traversal after insertion";
-    printInorder(root);
+    // int key  =  7;
+    // root = InsertNode(root,key);
+    // cout<<"Inorder traversal after insertion";
+    // printInorder(root);
 
-    return 0;
+    cout<<"level order traversal of binary tree is \n";
+    printLevelOrder(root);
+
+
+    // int key =2 ;
+    // root= deletion(root, key );
+    // cout<<endl;
+    // cout<<"inorder traversal after deletion";
+    // printInorder(root);
+
+    // return 0;
 
 }
-
-
