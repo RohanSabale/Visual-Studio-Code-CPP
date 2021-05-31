40f0b3f29e8f22ed6cb3aa7abebfa773dc6c124a
Rohan <sabalerohan73@gmail.com>
2021-05-31 19:08:22 +0530

    commit

--- babbar/Trees/BST_array.cpp
+++ babbar/Trees/BST_array.cpp
@@ -1,7 +1,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+struct Node{
+    int data;
+    Node*left;
+    Node*right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+Node* sortedArrayToBst(int arr[] , int start , int end)
+{
+    if(start>end)
+    {
+        return NULL;
+    }
+    int mid = (start + end)/2;
+    Node*root = new Node(arr[mid]);
+
+    root->left = sortedArrayToBst(arr , start , mid-1);
+
+    root->right = sortedArrayToBst(arr , mid+1 ,end);
+
+    return root;
+}
+
+void preorderPrint(Node* root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    cout<<root->data<<" ";
+    preorderPrint(root->left);
+    preorderPrint(root->right);
+}
+
 int main()
 {
-    
+    int arr[] = {10,20,30,40,50};
+    Node*root = sortedArrayToBst(arr,0,4);
+    preorderPrint(root);
+    cout<<endl;
+    return 0;
 }
