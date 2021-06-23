73ab0099de050b43c8403b5ea86cff3642393db5
Rohan <sabalerohan73@gmail.com>
2021-06-23 19:39:39 +0530

    commit

---
+++ babbar/Backtracking/intro.cpp
@@ -0,0 +1,38 @@
+/*
+backtracking - algorithmic - technique for solving problems recursively
+                        -try to build solution incrementally
+                        -remove those solutions that fail to satisfy the constraint of
+                         the problem.
+
+            1. do
+            2.recursion
+            3.undo
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void findPermutations(string s , int left , int right)
+{
+    for(int i = left;i<=right;i++)
+    {
+        // swapping done
+        swap(s[left] , s[right]);
+        
+
+        // recursion called
+        findPermutations(s , left+1 , right);
+        
+        //backtrack
+        swap(s[left] , s[i]);
+    }
+}
+
+int main()
+{
+    string s ;
+    cin>>s;
+    int n = s.size();
+    findPermutations(s , 0 , n);
+    return 0;
+}
