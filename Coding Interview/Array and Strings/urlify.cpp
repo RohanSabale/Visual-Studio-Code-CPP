9981da60fb3281febfab9a428a9975e5367d458b
Rohan <sabalerohan73@gmail.com>
2021-07-09 20:30:45 +0530

    commit

--- Coding Interview/Array and Strings/urlify.cpp
+++ Coding Interview/Array and Strings/urlify.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 void urlify(string s)
 {
-    
+    for(int i = 0;i<s.size();i++)
+    {
+        int k = int(s[i]);
+        if(k ==32)
+        {
+            s[i] = "%20";
+        }
+    }
+    cout<<s;
 }
 int main()
 {
