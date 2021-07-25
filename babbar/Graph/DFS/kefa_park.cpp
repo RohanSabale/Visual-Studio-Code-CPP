c921fb62ff7cdd7e4a0e04b716381d1b19be2095
Rohan <sabalerohan73@gmail.com>
2021-07-25 15:41:19 +0530

    commit

---
+++ babbar/Graph/DFS/kefa_park.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    #endif
+
+    int n , m;
+    cin>>n>>m;
+    vector<vector<int>>edges(n);
+    
+    int arr[n];
+    int vis[n];
+
+    for(int i = 0;i<n ;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i = 0;i<n-1 ;i++)
+    {
+        int l , r;
+        cin>>l>>r;
+        edges[l-1].push_back(r-1);    // l-1 and r-1 because of 0 based indexing
+        edges[r-1].push_back(l-1);
+    }
+
+    int count = 0;
+    if(arr[0] == 1)
+    {
+        count = 1;
+    }
+    dfs(edges , 0 , vis , m , count , arr);
+
+    int ans = 0;
+    for(int i = 0;i<n ;i++)
+    {
+        if(vis[i] == 1 && edges[i].size() == 1  && i!=0)    // edges[i].size() == 1 because leaf has only one parent above
+        // i!= 0 means it should be not root node
+        {
+            
+
+        }
+    }
+
+
+
+    return 0;
+
+}
