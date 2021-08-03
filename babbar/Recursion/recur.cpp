cf645339f7730729356368bf0787dfd96987d8de
Rohan <sabalerohan73@gmail.com>
2021-08-03 12:53:02 +0530

    commit

---
+++ babbar/Recursion/recur.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n)
+{
+    if(n==0)
+    {
+        return;
+    }
+    else
+        cout<<n;
+        print(n-1);
+}
+
+int main()
+{
+    int n;
+    n = 8;
+    print(n);
+}
