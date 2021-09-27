b2a1a6b767f8d614f20c71a9fac8b1a45be6c531
Rohan <sabalerohan73@gmail.com>
2021-09-27 16:51:10 +0530

    commit

--- Capgemini/hello.cpp
+++ Capgemini/hello.cpp
@@ -1,9 +1,35 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+const int N = 1e7 +10;
+
+int hsh[N];
+
+int length(char str[] , int n)
+{
+    for(int i = 0;i<n;i++)
+    {
+        int k = int(str[i]);
+        hsh[k]++;
+    }
+    int count = 0;
+    for(auto v: hsh)
+    {
+        if(v == 1)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
 int main()
 {
-    int chArr[] = {91,93,95};
-    cout<<*chArr+1;
-    return 0;
+   string s;
+getline(cin,s);
+
+    int len = s.size();
+    char *c = &s[0];
+
+   cout<<length(c, len);
+
 }
